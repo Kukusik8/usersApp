110199933f2982391a99f7f25c771379892d4ef3
Ruslan Sakhautdinov <sakhrus1406199@gmail.com>
2023-04-05 14:53:27 +0700

    ю12

--- src/App.js
+++ src/App.js
@@ -2,38 +2,26 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
-import {index} from './components/Users'
+
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   const [users,setUsers] = React.useState([]);
-  const [isLoading,setLoading] = React.useState([true]);
-  const [searchValue,setSearchValue] = React.useState('');
-  React.useEffect(()=> {
-    fetch ('https://reqres.in/api/users')
-      .then((res)=> res.json())
-      .then((json) =>{
-        setUsers(json.data);
-      }).catch((err) => {
-        console.warn(err);
-        alert ('Ошибка при получении пользователя')
-      })
-      .finally(() => setLoading(false));
-  },[])
 
-  const onChangeSearchValue = (event) => {
-    setSearchValue (event.target.value);
-  };
-
-  return (
-    <div className="App">
-      <Users onChangeSearchValue={onChangeSearchValue} 
-     searchValue={searchValue }
-     items ={users} 
-     isLoading={isLoading} />
-      {/* <Success /> */}
-    </div>
-  );
+  React.useEffect(()=>{
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data);
+    }).catch(err =>{
+      console.warn(err);
+      alert('Ошибка при получении данных');
+    })
+  },[]);
+ return (
+  <div className='App' >
+    <Users/>
+  </div>
+ ) ;
 }
-
 export default App;
